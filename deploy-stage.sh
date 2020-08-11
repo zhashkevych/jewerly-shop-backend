df7fce4095617ccb1931613d8fec88dc06fe1f4f
zhashkevych <zhashkevychmaksim@gmail.com>
2020-08-11 15:17:35 +0300

    added .env file support

--- deploy-stage.sh
+++ deploy-stage.sh
@@ -1,6 +1,5 @@
 git checkout -f develop && git pull origin develop
 
-export HOST=stage
 docker image build -t jewerly-api:0.1 -f ./deploy/Dockerfile .
 
 if [ "$(docker ps -q -f name=jewerly-api-stage)" ]; then
@@ -9,4 +8,4 @@ if [ "$(docker ps -q -f name=jewerly-api-stage)" ]; then
     fi
 fi
 
-docker run -e HOST -e ACCESS_KEY -e SECRET_KEY -e POSTGRES_PASSWORD -e PAYMENT_API_KEY -e PAYMENT_API_SECRET --rm -d --publish 8001:8000 --name jewerly-api-stage --link=jewerly-db:db jewerly-api:0.1
+docker run --env_file ../.env.prod --rm -d --publish 8001:8000 --name jewerly-api-stage --link=jewerly-db:db jewerly-api:0.1
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 git checkout -f master && git pull origin master
 
-export HOST=prod
 docker image build -t jewerly-api:0.1 -f ./deploy/Dockerfile .
 
 if [ "$(docker ps -q -f name=jewerly-api)" ]; then
@@ -9,4 +8,4 @@ if [ "$(docker ps -q -f name=jewerly-api)" ]; then
     fi
 fi
 
-docker run -e HOST -e ACCESS_KEY -e SECRET_KEY -e POSTGRES_PASSWORD -e PAYMENT_API_KEY -e PAYMENT_API_SECRET --rm -d --publish 8000:8000 --name jewerly-api --link=jewerly-db:db jewerly-api:0.1
+docker run --env_file ../.env.prod --rm -d --publish 8000:8000 --name jewerly-api --link=jewerly-db:db jewerly-api:0.1

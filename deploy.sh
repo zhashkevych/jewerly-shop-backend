7d52fb4f3a3ee55d1d70672be5c218d3fc5c17c2
Maksim Zhashkevych <zhashkevychmaksim@gmail.com>
2020-10-19 17:05:43 +0300

    updated deployment sh scripts

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ docker image build -t jewerly-api:0.1 -f ./deploy/Dockerfile .
 
 if [ "$(docker ps -q -f name=jewerly-api)" ]; then
     if [ ! "$(docker ps -aq -f status=exited -f name=jewerly-api)" ]; then
-        docker stop jewerly-api
+        docker rm $(docker stop jewerly-api)
     fi
 fi
 
-docker run --env-file ../.env.prod -v /root/jewerly-shop/api/logs/prod:/logs --rm -d --restart always --publish 8000:8000 --name jewerly-api --link=jewerly-db:db jewerly-api:0.1
+docker run --env-file ../.env.prod -v /root/jewerly-shop/api/logs/prod:/logs -d --restart always --publish 8000:8000 --name jewerly-api --link=jewerly-db:db jewerly-api:0.1
